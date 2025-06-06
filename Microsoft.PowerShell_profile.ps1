a7503de6c1368a356ca049eebb4531d525ac2e04
fliqlo <128495492+fliqlo@users.noreply.github.com>
2025-06-06 15:42:46 +0800

    Add files via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,11 @@
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerlevel10k_rainbow.omp.json" | Invoke-Expression
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+Import-Module -Name Terminal-Icons
